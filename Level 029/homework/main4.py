9f615cf30788e3da803e54cd950a8521758ddbc9
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2025-10-09 05:52:33 -0700

    commit

--- Level 029/homework/main4.py
+++ Level 029/homework/main4.py
@@ -1,7 +1,10 @@
 '''4) შექმენი ფუნქცნია lion4() სადაც შევქმნით ცარიელ მასივს და for loop ის გამოყენეით + .appnd() შევიტანთ მხოლოდ კენტ რიცხვებს'''
 
 def lion4(arr1):
+    # function
     for x in range(1,10,2):
+        # iteration for numbers
         arr1.append(arr1)
+        # add value in end list
 # calling a function
 print(lion4([]))
